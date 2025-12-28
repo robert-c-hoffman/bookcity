3a3addcac5fa46a230103fe555943ef89660796b
Pedro Silva <pedro.revez.silva@gmail.com>
2025-12-28 09:58:47 +0000

    Fix CSP: add bare archive.org domain for image redirects (#13)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self
     policy.font_src    :self, :data
-    policy.img_src     :self, :data, "https://covers.openlibrary.org", "https://*.archive.org"
+    policy.img_src     :self, :data, "https://covers.openlibrary.org", "https://archive.org", "https://*.archive.org"
     policy.object_src  :none
     policy.script_src  :self
     policy.style_src   :self, :unsafe_inline  # Required for Tailwind and inline SVG styles
